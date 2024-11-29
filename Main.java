7591fe6b6c9c9b3c3fc3f4835533991cfe72bd71
KsushaGruzdeva <135704686+KsushaGruzdeva@users.noreply.github.com>
2024-11-29 12:51:27 +0300

    update Main.java file for third commit

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ class Main {
         System.out.print("Это добавлено в ветке main");
         System.out.print("Коммит 1");
         System.out.print("Коммит 2");
+        System.out.print("Коммит 3");
     }
 }
