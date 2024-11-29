a9018f170ae41370c218114deea2918b9c5c9d1c
KsushaGruzdeva <135704686+KsushaGruzdeva@users.noreply.github.com>
2024-11-29 13:32:35 +0300

    update Main.java file for second commit from dev

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Коммит 1 из dev");
+        System.out.println("Коммит 2 из dev");
     }
 }
