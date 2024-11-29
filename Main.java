1c4e48ba421101117d089a0cc0c517e9b0289608
KsushaGruzdeva <135704686+KsushaGruzdeva@users.noreply.github.com>
2024-11-29 13:33:27 +0300

    update Main.java file for fourth commit from dev

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ class Main {
         System.out.println("Коммит 1 из dev");
         System.out.println("Коммит 2 из dev");
         System.out.println("Коммит 3 из dev");
+        System.out.println("Коммит 4 из dev");
     }
 }
